8417171509cc16c780d868cdce26042e3595b77e
cloud <cloud@pink-cvm02.novalocal>
2020-07-20 11:42:56 +0200

    rotation log

--- mqtel.py
+++ mqtel.py
@@ -12,24 +12,34 @@ TL = json.load(open(TLpath, "r"))
 ## bot setup
 bot = telegram.Bot(TL['token'])
 
-## tail setup
+## file setup
 fpath = "/var/log/auth.log"
+fsizelast = os.stat(fpath).st_size 
 
 authlog = open(fpath, 'r')
 dump = authlog.read()
 del dump
 
+## date setup
 print("Tailing file: " + fpath)
 print("running ...")
 
 try:
     while(1):
+        fsizenow = os.stat(fpath).st_size
+        if os.stat(fpath).st_size < fsizelast:
+            fsizelast = fsizenow
+            authlog.close()
+            authlog = open(fpath, 'r')
+            dump = authlog.read()
+            del dump
+
         newline = authlog.read()
         if len(newline)>3:
             if "cron:session" in newline:
                 continue
             try:
-                #print(newline, end="")
+                print(newline)
                 bot.sendMessage(TL['chatid'], newline)
             except:
                 print("failed to post message on TL")
